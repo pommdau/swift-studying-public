9118630d7f0b045cc6c8a7fc0f8c0c1b0eedf693
IKEH1024 <pondau1024@gmail.com>
2024-12-30 04:51:02 +0900

    chore: create project

---
+++ swift-zoomin/2024-10-29_Swift 6とStrictConcurrencyで困らないiOSアプリの作り方/Zoomin/Zoomin/ZoominApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZoominApp.swift
+//  Zoomin
+//
+//  Created by HIROKI IKEUCHI on 2024/12/30.
+//
+
+import SwiftUI
+
+@main
+struct ZoominApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
